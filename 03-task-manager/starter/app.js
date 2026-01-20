dc52bbafc9d440054c6af7dc1933f5e7e161f658
Soham Das <jisusoham04@gmail.com>
2026-01-20 17:58:20 +0530

    update app.js

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -1,1 +1,16 @@
-console.log('Task Manager App')
+import dotenv from "dotenv"
+dotenv.config();
+
+import express from "express"
+const app=express();
+
+const port=process.env.PORT;
+
+app.get('/',(req,res)=>{
+    res.send("Home page")
+})
+
+app.listen(port,()=>{
+    console.log(`The server is running on http://localhost:${port}`);
+    
+});
