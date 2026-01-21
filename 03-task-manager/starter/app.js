085c27b5001e40213d8e476c13b18c0783fa354f
Soham Das <jisusoham04@gmail.com>
2026-01-21 18:43:37 +0530

    update add.js

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -2,13 +2,19 @@ import dotenv from "dotenv"
 dotenv.config();
 
 import express from "express"
+import { router } from "./routes/user.routes.js";
+
 const app=express();
+
+app.use(express.json())
 
 const port=process.env.PORT;
 
 app.get('/',(req,res)=>{
     res.send("Home page")
 })
+
+app.use('/api/v1/users',router)
 
 app.listen(port,()=>{
     console.log(`The server is running on http://localhost:${port}`);
